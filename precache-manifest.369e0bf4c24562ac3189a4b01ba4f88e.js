be820247b91cbc7eb5ec85a94e80dec95c9bcdd2
Luan Eduardo da Costa <luan1346@gmail.com>
2020-03-28 23:36:11 -0300

    Updates

---
+++ precache-manifest.369e0bf4c24562ac3189a4b01ba4f88e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75aa2a243dc132adec6c046ac3b4ea57",
+    "url": "/carrijeta-sql-formatter/index.html"
+  },
+  {
+    "revision": "31658dd220abe50be910",
+    "url": "/carrijeta-sql-formatter/static/js/2.87d45a38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/carrijeta-sql-formatter/static/js/2.87d45a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2596f9168a76564e622",
+    "url": "/carrijeta-sql-formatter/static/js/main.757cb3e3.chunk.js"
+  },
+  {
+    "revision": "41b7edb98b77348e4481",
+    "url": "/carrijeta-sql-formatter/static/js/runtime-main.d21abbb7.js"
+  }
+]);
